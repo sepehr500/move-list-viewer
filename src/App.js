30c152d24fd1702d6b1eab0277f76a55b09b3be4
Sepher Sobhani <sepehr411@gmail.com>
2019-06-12 18:33:33 -0400

    Ability to make requests

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { MainPage } from "./pages/MainPage";
 import "./App.css";
+// Tachyons can be optimized for size later
+import "tachyons";
 
 function App() {
   return <MainPage />;
